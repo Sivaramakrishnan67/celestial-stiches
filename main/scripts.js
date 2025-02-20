0c02ccb38f6e420f3f82b4c930d63fee6ab2c71d
Sivaramakrishnan67 <144478647+Sivaramakrishnan67@users.noreply.github.com>
2025-02-20 10:52:47 +0530

    Add files via upload

---
+++ main/scripts.js
@@ -0,0 +1,10 @@
+//header
+let nav = document.querySelector("nav");
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+  }else {
+    nav.classList.remove("sticky");
+  }
+}
+//advertisement
